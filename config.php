4e19e5fc5b1c80ac4c8bdbdcc2b578f2369718e9
Shoaib Nusrat <nusratshoaibansarit@gmail.com>
2020-06-28 10:28:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Defining CONSTANT variable. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'mkaliqip_mansur');
-define('DB_PASSWORD', 'Alig_123');
-define('DB_NAME', 'mkaliqip_virtual_lab');//enter your db name inplace of demo
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'virtual_lab');//enter your db name inplace of demo
  
 // Connecting Database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
